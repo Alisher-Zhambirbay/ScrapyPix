6bf7aa63f529407d8de3c3b611bcb08e5c947ad6
Alisher Zhambirbay <113189134+Alisher-Zhambirbay@users.noreply.github.com>
2024-11-17 06:07:24 +0500

    Update check.py
    - Removed PATH features and etc.
    - Fixed installation function

--- check.py
+++ check.py
@@ -1,6 +1,5 @@
 import os
 import sys
-import ctypes
 import subprocess
 from datetime import datetime
 
@@ -10,19 +9,18 @@ def is_win_platform():
 def is_other_platform():
     return sys.platform in ["darwin", "linux", "linux2"]
 
-def is_admin():
+def __hide_file__(file_path):
     if is_win_platform():
         try:
-            return ctypes.windll.shell32.IsUserAnAdmin() != 0
-        except:
-            return False
-        
-    elif is_other_platform():
-        return os.geteuid() == 0
+            subprocess.run(["attrib", "+h", "+s", file_path], check=True)
+            print(f"Hidden file on Windows: {file_path}")
+        except Exception as e:
+            print(f"Failed to hide file on Windows: {e}")
 
 def installs():
     """
     Checks installed requirements and saves the installation date if successful.
+    This function is designed to work on both Windows and Unix-based systems.
     """
     requirements = "requirements.txt"
     installed_marker = "installed.txt"
@@ -34,20 +32,28 @@ def installs():
             with open(installed_marker, 'r') as file:
                 install_date = file.read().strip()
                 print(f"Last installation date: {install_date}")
+
             return 1
-        
+
         try:
+            python_executable = "python3" if is_other_platform() else "py"
+
             subprocess.call([
-                "py", "-m", "pip", "install", "-r", "requirements.txt"
+                python_executable, "-m", "pip", "install", "-r", requirements
             ])
             
             with open(installed_marker, 'w') as file:
                 file.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 
+            __hide_file__(installed_marker)
+
+            print("Requirements installed successfully.")
             return 1
+
         except Exception as exception:
             print(f"Error in installs: {exception}")
             return 0
+
     else:
         print(f"Cannot find the requirement file ({requirements}).")
         return 0
