b582a658c752e144f09e5b3d02f893cb21ce53d6
Alisher Zhambirbay <113189134+Alisher-Zhambirbay@users.noreply.github.com>
2024-11-17 06:21:51 +0500

    Update logger.py
    Changed to if-else for better compatibility

--- logger.py
+++ logger.py
@@ -1,4 +1,6 @@
-from colorama import Fore
+from colorama import Fore, just_fix_windows_console
+
+just_fix_windows_console() # oh yeah
 
 INFO = 1
 WARN = 2
@@ -7,18 +9,22 @@ SUCCESS = 4
 DEBUG = 5
 
 def log(message="", type: int = DEBUG):
+    """
+    :param message: Message to print
+    :param type: Type of message (INFO, WARN, ERROR, SUCCESS, DEBUG)
+    """
     type_text = f"{Fore.LIGHTWHITE_EX}{message}"
+    
     if message:
-        match type:
-            case 1:
-                type_text = f"{Fore.CYAN}[INFO] {message}"
-            case 2:
-                type_text = f"{Fore.YELLOW}[WARN] {message}"
-            case 3:
-                type_text = f"{Fore.RED}[ERROR] {message}"
-            case 4:
-                type_text = f"{Fore.GREEN}[SUCCESS] {message}"
-            case 5:
-                type_text = f"{Fore.LIGHTWHITE_EX}{message}"
+        if type == INFO:
+            type_text = f"{Fore.CYAN}[INFO] {message}"
+        elif type == WARN:
+            type_text = f"{Fore.YELLOW}[WARN] {message}"
+        elif type == ERROR:
+            type_text = f"{Fore.RED}[ERROR] {message}"
+        elif type == SUCCESS:
+            type_text = f"{Fore.GREEN}[SUCCESS] {message}"
+        elif type == DEBUG:
+            type_text = f"{Fore.LIGHTWHITE_EX}{message}"
 
     print(type_text, Fore.RESET)
